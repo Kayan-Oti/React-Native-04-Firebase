d260c36f6205e3f52acf715a374bbf7c4dd2e780
Kayan-Oti <kayanoti@gmail.com>
2020-11-16 15:30:06 +0000

    React-Native-04-Firebase
    Signed-off-by: Kayan-Oti <kayanoti@gmail.com>

--- Colecionaveis03/src/views/Item/Item.js
+++ Colecionaveis03/src/views/Item/Item.js
@@ -1,22 +1,106 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import estiloItem from './estiloItem';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
 
-function Item({ navigation }) {
+function Item({ navigation, route }) {
+
+    const [item, setItem] = useState('');
+    const {operacao,setOperacao} = route.params;
+
+    useEffect(() => {
+        setItem(route.params.item);
+    }, [route.params.item]);
 
     const voltar = () => {
         navigation.navigate('Inicial')
     }
     
+    const salvar = () => { }
+
+    const remover = () => { }
+
     return (
         <View style={estiloItem.container}>
-            <View style={estiloItem.borda}>
 
-                <Text style={estiloItem.texto}>Item</Text>
-
+            <View style={estiloItem.header}>
                 <TouchableOpacity style={estiloItem.botaoContainer} onPress={voltar}>
-                    <Text style={estiloItem.botaoTexto}>Voltar</Text>
+                    <MaterialIcons name="arrow-back" size={24} color="white" />
                 </TouchableOpacity>
+                <Text style={estiloItem.texto}>Item</Text>
+                <Text></Text>
+            </View>
+
+            <View style={estiloItem.formularioContainer}>
+
+                <View style={estiloItem.campoContainer}>
+                    <FontAwesome5 name="book" size={26} color="#192f6a" />
+                    <TextInput 
+                        style={estiloItem.campo}
+                        placeholder="Titulo"
+                        placeholderTextColor='#FFFFFF'
+                        onChangeText={titulo => setItem(...item, titulo)}
+                        value={item.titulo}
+                    />
+                </View>
+
+                <View style={estiloItem.campoContainer}>
+                    <FontAwesome5 name="book-reader" size={26} color="#192f6a" />
+                    <TextInput 
+                        style={estiloItem.campo}
+                        placeholder="Autor"
+                        placeholderTextColor='#FFFFFF'
+                        onChangeText={Autor => setItem(...item, Autor)}
+                        value={item.Autor}
+                    />
+                </View>
+
+                <View style={estiloItem.campoContainer}>
+                    <FontAwesome5 name="calendar-alt" size={26} color="#192f6a" />
+                    <TextInput 
+                        style={estiloItem.campo}
+                        placeholder="Ano de publicação"
+                        placeholderTextColor='#FFFFFF'
+                        kayboardType='numeric'
+                        maxLength={4}
+                        onChangeText={anoPublicacao => setItem(...item, anoPublicacao)}
+                        value={item.anoPublicacao ? item.anoPublicacao.toString() : item.anoePublicacao}
+                    />
+                </View>
+
+                <View style={estiloItem.campoContainer}>
+                    <FontAwesome5 name="book" size={26} color="#192f6a" />
+                    <ScrollView style={estiloItem.campoDescricaoScroll}>
+                        <TextInput 
+                            style={estiloItem.campoDescricao}
+                            placeholder="Descrição"
+                            placeholderTextColor='#FFFFFF'
+                            multiline={true}
+                            numberOfLines={4}
+                            blurOnSubmit={false}
+                            onChangeText={descricao => setItem(...item, descricao)}
+                            value={item.descricao}
+                        />
+                    </ScrollView>
+                </View>
+
+                <View style={estiloItem.botoesContainer}>
+
+                    <TouchableOpacity onPress={salvar} style={estiloItem.botaoContainer}>
+                        <LinearGradient colors={['#4c669f', '#192f6a', '#081a31']} style={estiloItem.botao}>
+                            <MaterialIcons name="save" size={28} color="white" />
+                        </LinearGradient>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={remover} style={estiloItem.botaoContainer}>
+                        <LinearGradient colors={['#4c669f', '#192f6a', '#081a31']} style={estiloItem.botao}>
+                            <MaterialIcons name="delete" size={28} color="white" />
+                        </LinearGradient>
+                    </TouchableOpacity>
+
+                </View>
 
             </View>
         </View>
--- Colecionaveis03/src/views/Item/estiloItem.js
+++ Colecionaveis03/src/views/Item/estiloItem.js
@@ -3,40 +3,88 @@ import { StyleSheet } from 'react-native';
 const estiloItem = StyleSheet.create({
    container: {   
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',     
+      backgroundColor: '#92afd7',  
    },
-   borda: {   
-      backgroundColor: '#081a31',  
-      width: '80%',
-      height: '25%',
-      borderRadius: 10,
+   header: {
+       height: 50,
+       backgroundCOlor: '#14417b',
+       flexDirection: 'row',
+       alignItems: 'center',
+       justifyContent: 'space-between',
+       padding: 5,
+   },
+   formularioContainer: {
+       flex: 1,
+       justifyContent: 'center',
    },
    texto: {
       fontSize: 20,
       color: '#FFFFFF',
       fontWeight: 'bold',
-      textAlign: "center",
-      padding: 10
    },
-   botaoContainer: {
-      marginTop: 20,
-      marginBottom: 20,
-      marginLeft: 40,
-      marginRight: 40,
-      borderRadius: 5,
-      height: 40,
-      borderWidth: 2,
-      borderColor: '#e8f2e9',
-      backgroundColor: '#14417b',
-      justifyContent: 'center',
+
+   campoContainer: {   
+      flexDirection: 'row',
+      padding: 5,
+      paddingLeft: 50,
+      paddingRight: 50,
+      margin: 5,
+      marginRight: 50,
       alignItems: 'center',
    },
-   botaoTexto: {
-      fontSize: 18,
-      color: '#FFFFFF',
-      fontWeight: 'bold'
+
+    campoContainerDescricao: {   
+      flexDirection: 'row',
+      padding: 5,
+      paddingLeft: 45,
+      paddingRight: 40,
+      margin: 5,
    },
+   campo: {
+       width: '100%',
+       fontSize: 18,
+       color: '#FFF',
+       backgroundColor: '#4c669f',
+       marginLeft: 15,
+       padding: 10,
+       borderWidth: 1,
+       borderColor: '#192f6a',
+       borderRadius: 5,
+   },
+   campoDescricao: {
+       width: '90%',
+       height: 100,
+       fontSize: 18,
+       color: '#FFF',
+       backgroundColor: '#4c669f',
+       marginLeft: 15,
+       padding: 10,
+       borderWidth: 1,
+       borderColor: '#192f6a',
+       borderRadius: 5,
+       textAlignVertical: 'top',
+   },
+   campoDescricaoScroll: {
+       width: '100%',
+   },
+   botoesContainer: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      paddingRight: 50,
+   },
+   botaoContainer: {
+       alignItems: 'center',
+       padding: 5,
+   },
+   botao: {
+       widht: 50,
+       height: 50,
+       borderRadius: 10,
+       justifyCOntent: 'center',
+       alignItems: "center",
+       padding: 10,
+   },
+
 });
 
 export default estiloItem;
